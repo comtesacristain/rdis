5d5518031c42306e27708de1e786c7942d21a73d
Michael Sexton <mick.sexton@gmail.com>
2014-10-15 12:44:54 +1100

    Added db settings

--- rdis/settings.py
+++ rdis/settings.py
@@ -57,22 +57,7 @@ WSGI_APPLICATION = 'rdis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'production': {
-        'ENGINE':'',
-	'NAME': '',
-	'USER': '',
-	'PASSWORD': '',
-	'HOST': '',
-	'PORT': ''
-    }
-}
+from database import *
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
