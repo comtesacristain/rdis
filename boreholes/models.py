d298a3dac45e516fdf080dd2eccc76887763080d
Michael Sexton <mick.sexton@gmail.com>
2014-10-15 17:04:10 +1100

    stupid indents

--- boreholes/models.py
+++ boreholes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Deposit(models.Model):
-	eno = models.AutoField(primary_key=True)
+eno = models.AutoField(primary_key=True)
     
     class Meta:
         db_table = 'mgd.deposits'
