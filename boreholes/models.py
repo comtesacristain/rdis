62b5534f0cbd88718b4ede97eed8032fe300ea5c
Michael Sexton <mick.sexton@gmail.com>
2014-10-16 14:20:46 +1100

    Added entity details and str

--- boreholes/models.py
+++ boreholes/models.py
@@ -11,6 +11,12 @@ class Deposit(models.Model):
 
 class Borehole(models.Model):
     eno = models.AutoField(primary_key=True)
+    entityid = models.TextField()
+    entity_type = models.TextField()
+    
+    def __str__(self):              # __unicode__ on Python 2
+        return self.entityid
     
     class Meta:
         db_table = '"a"."entities"'
+        
