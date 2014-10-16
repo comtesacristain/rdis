717faa77961ef0a6ef427d888998f1a9023c56a1
Michael Sexton <mick.sexton@gmail.com>
2014-10-16 14:08:52 +1100

    Mexican ninja rabbits

--- boreholes/models.py
+++ boreholes/models.py
@@ -6,15 +6,11 @@ class Deposit(models.Model):
     eno = models.AutoField(primary_key=True)
     
     class Meta:
-<<<<<<< HEAD
         db_table = '"mgd"."deposits"'
-=======
-        db_table = 'mgd.deposits'
 
 
 class Borehole(models.Model):
     eno = models.AutoField(primary_key=True)
     
     class Meta:
-        db_table = 'a.entities'
->>>>>>> 7f765c0ad062cb2e08e52a66b079244a0cf15939
+        db_table = '"a"."entities"'
