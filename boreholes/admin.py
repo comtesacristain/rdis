3a76d725ea61188f1f22554215e0d678132785db
Michael Sexton <mick.sexton@gmail.com>
2015-03-04 16:35:19 +1100

    Added models

--- boreholes/admin.py
+++ boreholes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import * 
+admin.site.register(Borehole)
+admin.site.register(Deposit)
+admin.site.register(Province)
+admin.site.register(Survey)
 # Register your models here.
