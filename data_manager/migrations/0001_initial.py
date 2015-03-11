62074d24158e6c0fab5fe09b0825e96bb36560bd
Michael Sexton <mick.sexton@gmail.com>
2015-03-11 15:11:13 +1100

    Migrations

---
+++ data_manager/migrations/0001_initial.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Duplicate',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('table_id', models.IntegerField(null=True)),
+                ('table_name', models.TextField(null=True)),
+                ('entity_type', models.TextField(null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='DuplicateType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('kind', models.TextField(null=True)),
+                ('field', models.TextField(null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='duplicate',
+            name='duplicate',
+            field=models.ForeignKey(to='data_manager.DuplicateType', null=True),
+            preserve_default=True,
+        ),
+    ]
