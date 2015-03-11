b99e9c998906f214866d5f87a0fc855cdb7a43c0
Michael Sexton <mick.sexton@gmail.com>
2015-03-11 15:12:28 +1100

    Changed entity types

--- boreholes/admin.py
+++ boreholes/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 from .models import * 
-admin.site.register(Borehole)
+#admin.site.register(Well)
+#admin.site.register(Drillhole)
 admin.site.register(Deposit)
 admin.site.register(Province)
 admin.site.register(Survey)
