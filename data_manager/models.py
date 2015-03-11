ae57d0bb2e43dea9a2bc112ad25a8eba9498fd83
Michael Sexton <mick.sexton@gmail.com>
2015-03-11 15:10:42 +1100

    Added attributes to Duplicate model, removed model field from DuplicateType

--- data_manager/models.py
+++ data_manager/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from boreholes.models import Drillhole, Well
 
 # Create your models here.
 class Duplicate(models.Model):
-    table_id = models.IntegerField()
-    table_name = models.TextField()
-    duplicate = models.ForeignKey("DuplicateType")
+    table_id = models.IntegerField(null=True)
+    table_name = models.TextField(null=True)
+    entity_type = models.TextField(null=True)
+    duplicate = models.ForeignKey("DuplicateType",null=True)
 
 class DuplicateType(models.Model):
-    kind = models.TextField()
-    field = models.TextField()
-    model = models.TextField()
+    kind = models.TextField(null=True)
+    field = models.TextField(null=True)
     
