bf02b757d8723a21e280c7099706b051a2f39c99
Michael Sexton <mick.sexton@gmail.com>
2015-03-23 12:53:31 +1100

    Added views

--- data_manager/views.py
+++ data_manager/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from data_manager.models import *
+
+# Create your views here.
+def index(request):
+    duplicate_groups= DuplicateGroup.objects.all()
+    
+    return render(request, 'duplicates/index.html', {"duplicate_groups":duplicate_groups})
+    #return HttpResponse("test")
+	
+def duplicate(request, id):
+    duplicate_group= DuplicateGroup.objects.get(pk=id)
+	duplicates = duplicate_group.duplicate_set.all() 
+    return render(request, 'duplicates/duplicate.html', {"duplicate_group":duplicate_group,"duplicates":duplicates})
