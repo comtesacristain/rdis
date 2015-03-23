18210fc1609d2d3b63b48dd6d5fb826cf99bb2b0
Michael Sexton <mick.sexton@gmail.com>
2015-03-23 12:53:15 +1100

    Changed DuplicateType to DuplicateGroup

--- data_manager/models.py
+++ data_manager/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-from boreholes.models import Drillhole, Well
 
 # Create your models here.
 class Duplicate(models.Model):
     table_id = models.IntegerField(null=True)
     table_name = models.TextField(null=True)
     entity_type = models.TextField(null=True)
-    duplicate = models.ForeignKey("DuplicateType",null=True)
+    duplicate_group = models.ForeignKey("DuplicateGroup",null=True)
 
-class DuplicateType(models.Model):
+class DuplicateGroup(models.Model):
     kind = models.TextField(null=True)
     field = models.TextField(null=True)
     
