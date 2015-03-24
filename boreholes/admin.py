9442f60be1e39f157967b4243f0c8508a5c8284e
Michael Sexton <mick.sexton@gmail.com>
2015-03-24 13:50:23 +1100

    Changed registered models

--- boreholes/admin.py
+++ boreholes/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import * 
 #admin.site.register(Well)
 #admin.site.register(Drillhole)
-admin.site.register(Deposit)
-admin.site.register(Province)
-admin.site.register(Survey)
+admin.site.register(Entity)
+#admin.site.register(Province)
+#admin.site.register(Survey)
 # Register your models here.
