ae9f9a01d660dbf776672e98aedd0575f88df790
Michael Sexton <mick.sexton@gmail.com>
2015-03-24 15:59:37 +1100

    CHanged whitespace

--- data_manager/views.py
+++ data_manager/views.py
@@ -6,11 +6,10 @@ from data_manager.models import *
 # Create your views here.
 def index(request):
     duplicate_groups= DuplicateGroup.objects.all()
-    
     return render(request, 'duplicates/index.html', {"duplicate_groups":duplicate_groups})
     #return HttpResponse("test")
 	
 def duplicate(request, id):
     duplicate_group= DuplicateGroup.objects.get(pk=id)
-	duplicates = duplicate_group.duplicate_set.all() 
+    duplicates = duplicate_group.duplicate_set.all() 
     return render(request, 'duplicates/duplicate.html', {"duplicate_group":duplicate_group,"duplicates":duplicates})
