e4997f2d113d54c01e543eacc66aef298fbe3e0c
Michael Sexton <mick.sexton@gmail.com>
2015-03-25 13:46:37 +1100

    Tidy up

--- data_manager/views.py
+++ data_manager/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from data_manager.models import *
+from boreholes.models import *
+
 
 # Create your views here.
 def index(request):
@@ -12,4 +14,6 @@ def index(request):
 def duplicate(request, id):
     duplicate_group= DuplicateGroup.objects.get(pk=id)
     duplicates = duplicate_group.duplicate_set.all() 
+    enos = [x[0] for x in duplicates.values_list('table_id')]
+    duplicate_entities=Entity.objects.filter(eno__in=enos)
     return render(request, 'duplicates/duplicate.html', {"duplicate_group":duplicate_group,"duplicates":duplicates})
