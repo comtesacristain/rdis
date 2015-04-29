47b84a7db654a58aaf340dbb4565306da1a6ff33
Michael Sexton <mick.sexton@gmail.com>
2015-04-29 13:58:35 +1000

    Changes to installed apps

--- rdis/settings.py
+++ rdis/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    'boreholes'
+    'boreholes',
+    'data_manager'
 )
 
 MIDDLEWARE_CLASSES = (
