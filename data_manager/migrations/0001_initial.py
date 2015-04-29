2d0020ac8925f77af306581f3a16dae68c28cfff
Michael Sexton <mick.sexton@gmail.com>
2015-04-29 13:51:44 +1000

    Reworked data_manager migrations

--- data_manager/migrations/0001_initial.py
+++ data_manager/migrations/0001_initial.py
@@ -14,9 +14,16 @@ class Migration(migrations.Migration):
             name='Duplicate',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('table_id', models.IntegerField(null=True)),
+                ('eno', models.IntegerField(null=True)),
                 ('table_name', models.TextField(null=True)),
                 ('entity_type', models.TextField(null=True)),
+                ('entityid', models.TextField(null=True)),
+                ('x', models.FloatField(null=True)),
+                ('y', models.FloatField(null=True)),
+                ('z', models.FloatField(null=True)),
+                ('has_well', models.BooleanField(default=False)),
+                ('has_samples', models.BooleanField(default=False)),
+                ('no_samples', models.IntegerField(null=True)),
             ],
             options={
             },
@@ -28,6 +35,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('kind', models.TextField(null=True)),
                 ('field', models.TextField(null=True)),
+                ('num_dupes', models.IntegerField(null=True)),
             ],
             options={
             },
