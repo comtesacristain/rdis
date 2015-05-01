2bc317c9a387646666b3aa86223a9e0dfc5ee472
Michael Sexton <mick.sexton@gmail.com>
2015-05-01 17:21:42 +1000

    Extra attributes and functions for the model

--- data_manager/models.py
+++ data_manager/models.py
@@ -14,7 +14,10 @@ class Duplicate(models.Model):
     has_well = models.BooleanField(default=False)
     has_samples = models.BooleanField(default=False)
     no_samples = models.IntegerField(null=True)
-    deletion_status = models.TextField(default='UN')
+    
+    action_status = models.TextField(default='UN')
+    deleted = models.TextField(default='N')
+    data_transferred_to = models.IntegerField(null=True)
     
     def __str__(self):
         return "{0}: {1}".format(self.eno,self.entityid)
@@ -26,6 +29,14 @@ class DuplicateGroup(models.Model):
     kind = models.TextField(null=True)
     field = models.TextField(null=True)
     num_dupes = models.IntegerField(null=True)
+    has_resolution = models.TextField(default='N')
+    qaed = models.TextField(default='N')
+    
+    def number_of_wells(self):
+        return self.duplicate_set.filter(entity_type='WELL').count()
+        
+    def number_of_drillholes(self):
+        return self.duplicate_set.filter(entity_type='DRILLHOLE').count()
     
     def __str__(self):
         return "{0}: {1}".format(self.id,self.kind)
